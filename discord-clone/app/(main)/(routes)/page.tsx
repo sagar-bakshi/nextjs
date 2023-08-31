f161c2b329aff91672cc20b0271608f8e9171cc1
Sagar Bakshi <sagar.bakshi@terumoamericas.com>
2023-09-01 02:30:07 +0530

    theme setup complete with toggle button

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
